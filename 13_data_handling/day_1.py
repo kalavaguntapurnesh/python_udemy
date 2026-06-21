ad99ddad5d1e00bab4356800c69a027ca9584b38
Purnesh Kalavagunta <purnesh.kalavagunta@clouddatanetworks.com>
2026-06-21 10:14:44 +0100

    Contacts Search using CSV

--- 13_data_handling/day_1.py
+++ 13_data_handling/day_1.py
@@ -1,2 +1,96 @@
 import csv
 
+import os
+
+FILENAME = "contacts.csv"
+
+if not os.path.exists(FILENAME):
+    with open(FILENAME, "w", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Name", "Phone", "Email"])
+        
+def add_contact():
+    name = input("Name: ").strip()
+    phone = input("Phone : ").strip()
+    email = input("Email : ").strip()
+    
+    # check for duplicates
+    
+    with open(FILENAME, 'r', encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row["Name"].lower() == name.lower():
+                print("Contact name already exists")
+                return
+    
+    with open(FILENAME, 'a', newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow([name, phone, email])
+        print("Contact added")
+
+
+def view_contacts():
+    with open(FILENAME, 'r', encoding="utf-8") as f:
+        reader = csv.reader(f)
+        rows = list(reader)
+        
+        if len(rows) < 1:
+            print("No Contacts found")
+            return
+        
+        print("\nYour contacts: \n")
+        
+        
+        for row in rows[1: ]:
+            
+            if len(row) < 3:
+                continue
+            
+            print(f"{row[0]} | {row[1]} | {row[2]}")
+        print()
+        
+def search_contact():
+    term = input("Enter the name to search : ").strip().lower()
+    found = False
+    
+    with open(FILENAME, 'r', encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if term in row["Name"].lower():
+                print(f"{row['Name']} | {row['Phone']}")
+                found = True
+                
+    if not found:
+        print("No matching contact found\n")
+    
+
+
+def main():
+    while True:
+        print("Contact Book\n")
+        print("1. Add Contact")
+        print("2. View All Contacts")
+        print("3. Search Contact")
+        print("4. Exit")
+        
+        choice = input("Choose an option (1-4)\n").strip()
+        
+        
+        if choice == "1":
+            add_contact()
+            
+        elif choice == "2":
+            view_contacts()
+        elif choice == "3":
+            search_contact()
+        elif choice == "4":
+            print("Thanks for using our software\n")
+            break
+        else:
+            print("Invalid choice of number\n")
+        
+
+
+if __name__ == "__main__":
+    main()
+        
